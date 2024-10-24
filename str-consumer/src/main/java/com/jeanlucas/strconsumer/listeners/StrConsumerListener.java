c3cf3d2ff3acefa6aa2bd8dcfdcdafa698e23ec1
jeanlucasbs <jean.silva@discente.ufg.br>
2024-10-24 18:28:16 -0300

    Created a custumer annotated interface

--- str-consumer/src/main/java/com/jeanlucas/strconsumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/jeanlucas/strconsumer/listeners/StrConsumerListener.java
@@ -1,15 +1,25 @@
 package com.jeanlucas.strconsumer.listeners;
 
+import com.jeanlucas.strconsumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
-    public void listener(String message) {
-        log.info("Receive message {}", message);
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void create(String message) {
+        log.info("CREATE ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-1")
+    public void log(String message) {
+        log.info("LOG ::: Receive message {}", message);
+    }
+
+    @StrConsumerCustomListener(groupId = "group-2")
+    public void history(String message) {
+        log.info("HISTORY ::: Receive message {}", message);
     }
 }
